c47d9d12275260c3faf1f3f583f5f43137ea825f
KOSA <KOSA@DESKTOP-6FO5ULH>
2022-11-24 19:54:41 +0900

    setting

---
+++ src/main/java/com/kosa/app/KosaApplication.java
@@ -0,0 +1,13 @@
+package com.kosa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KosaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KosaApplication.class, args);
+	}
+
+}

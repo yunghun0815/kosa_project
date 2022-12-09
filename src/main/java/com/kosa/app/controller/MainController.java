17544bf0efbdf988cd2e7d557523dafa08f5037b
Jin Lee <110071055+Jinlunalee@users.noreply.github.com>
2022-12-09 14:44:34 +0900

    update

--- src/main/java/com/kosa/app/controller/MainController.java
+++ src/main/java/com/kosa/app/controller/MainController.java
@@ -1,22 +1,48 @@
 package com.kosa.app.controller;
 
-import java.net.URI;
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMessage.RecipientType;
 
-import org.springframework.boot.autoconfigure.data.rest.RepositoryRestMvcAutoConfiguration;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponents;
-import org.springframework.web.util.UriComponentsBuilder;
 
 @Controller
 public class MainController {
+	@Autowired
+	JavaMailSender javaMailSender;
+	
 	@GetMapping("/")
 	public String main() {
 		return "index";
 	}
 	
+	@ResponseBody
+	@GetMapping("/mail")
+	public String sendMail() {
+		MimeMessage message = javaMailSender.createMimeMessage();
+		try {
+			//인증코드 1000 ~ 9999 랜덤한 숫자
+			int code = (int) (Math.random() * (9999-1000)) + 1000 ;
+			
+			
+			//메일 제목 설정
+			message.setSubject("제목~~");
+			
+			//메일 내용 설정
+			String text = "<div style=\"width: 100px; height: 100px; border: 1px solid black; text-align: center;\">인증코드 :" + code + "</div>";
+			message.setText(text, "utf-8", "html");
+			
+			//받는사람 설정
+			message.addRecipients(RecipientType.TO, "yunghun0815@gmail.com");
+			
+			javaMailSender.send(message);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+		return "SUCCESS";
+	}
 }
